0cec51e30cf6e3545a2388d7c4395b562f993c9c
Kevin Madhu <kevin.madhu@gmail.com>
2015-10-16 08:14:12 +0530

    Seed data for handling roles.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,16 @@
 unless User.where(profile_type: "Admin").count > 0
-  User.create(first_name: 'mentor', last_name: 'performance', 
+  #Create role for users
+
+  #Student Role
+  Role.create(name: 'Student', activities: ["examinations_menu", "exam_registrations:index", 
+    "exam_registrations:new", "exam_registrations:create", "exam_registrations:show"])
+
+  #Admin Role
+  @admin_role = Role.create(name: 'Admin', activities: ["students_menu", "settings_menu"])
+
+  #Admin user
+  @admin_user = User.new(first_name: 'mentor', last_name: 'performance', 
     email: 'info@mentorperformance.in', password: 'mentor123', profile_type: 'Admin')
+  @admin_user.roles << @admin_role
+  @admin_user.save
 end
