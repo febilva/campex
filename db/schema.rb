862813cae734ead5930abd8cb5ef27b0c06ffae1
Kevin Madhu <kevin.madhu@gmail.com>
2015-11-11 11:22:32 +0530

    Adds dob attribute for users.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151103073808) do
+ActiveRecord::Schema.define(version: 20151111043738) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -220,6 +220,7 @@ ActiveRecord::Schema.define(version: 20151103073808) do
     t.string   "last_name"
     t.string   "username"
     t.string   "dp"
+    t.date     "dob"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
