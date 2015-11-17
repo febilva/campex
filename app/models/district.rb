611518b793dcdc85f4a1cf3e4dfe742bb504fbbc
Kevin Madhu <kevin.madhu@gmail.com>
2015-11-17 11:50:01 +0530

    Modifies districts and state to display names when used as strings.

--- app/models/district.rb
+++ app/models/district.rb
@@ -1,3 +1,7 @@
 class District < ActiveRecord::Base
   belongs_to :state
+
+  def to_s
+    self.name
+  end
 end
--- app/models/state.rb
+++ app/models/state.rb
@@ -1,3 +1,7 @@
 class State < ActiveRecord::Base
   has_many :districts
+
+  def to_s
+    self.name
+  end
 end
