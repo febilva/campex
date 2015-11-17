6ed6105f6b88afbd88c584b0c8a2e33bc3e0f558
Kevin Madhu <kevin.madhu@gmail.com>
2015-11-17 10:04:24 +0530

    Adds seed data for states and districts.

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,4 +13,34 @@ unless User.where(profile_type: "Admin").count > 0
     email: 'info@mentorperformance.in', password: 'mentor123', profile_type: 'Admin')
   @admin_user.roles << @admin_role
   @admin_user.save
+
+  states = [
+    'Kerala', 'Tamilnadu', 'Andaman and Nicobar Islands', 'Andhra Pradesh',
+    'Arunachal Pradesh', 'Assam', 'Bihar', 'Chandigarh', 'Chhattisgarh', 
+    'Goa', 'Gujarat', 'Haryana', 'Himachal Pradesh', 'Jammu and Kashmir', 
+    'Jharkhand', 'Lakshadweep', 'Madhya Pradesh', 'Maharashtra', 'Manipur', 
+    'Meghalaya', 'Mizoram', 'Nagaland', 'Odisha', 'Puducherry', 'Punjab',
+    'Rajasthan', 'Sikkim', 'Telangana', 'Tripura', 'Uttar Pradesh',
+    'Uttarakhand', 'West Bengal', 'Karnataka'
+  ]
+
+  districts = {
+    'Kerala' => ['Alappuzha', 'Ernakulam', 'Idukki', 'Kannur', 'Kasaragod',
+      'Kollam', 'Kottayam', 'Kozhikode', 'Malappuram', 'Palakkad',
+      'Pathanamthitta', 'Thiruvananthapuram', 'Thrissur', 'Wayanad']
+  }
+
+  states.each do |state|
+    @state = State.new(name: state)
+    success = @state.save
+    if success
+      unless districts[state].nil?
+        districts[state].each do |district|
+          @district = District.new(state: @state,
+            name: district)
+          @district.save
+        end
+      end
+    end
+  end
 end
