aad12054d0dba93e12c15ecfb2799c8e5fb4a217
Kevin Madhu <kevin.madhu@gmail.com>
2015-12-04 15:04:16 +0530

    Defines a one2many relation btw timetable and timetable_entries.

--- app/models/timetable.rb
+++ app/models/timetable.rb
@@ -2,5 +2,6 @@ class Timetable < ActiveRecord::Base
   belongs_to :class_timing
   belongs_to :batch
   belongs_to :term_structure_entry
+  has_many :timetable_entries
   has_many :periods, through: :class_timing
 end
