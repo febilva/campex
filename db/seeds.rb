3168409583b2dd3bdd57387c14e328549ce8a5c2
Kevin Madhu <kevin.madhu@gmail.com>
2015-12-15 08:32:38 +0530

    Modifies seeds to grant teachers role a needed privilege.

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,7 +11,7 @@ unless User.where(profile_type: "Admin").count > 0
   #Teacher Role
   Role.create(name: 'Teacher', activities: ["attendance_menu", "student_attendance_registers:index", 
     "student_attendance_registers:new", "student_attendance_registers:create", 
-    "student_attendance_registers:show", "teachers:period_list"])
+    "student_attendance_registers:show", "teachers:period_list", "batches:students_list"])
 
   #Admin user
   @admin_user = User.new(username: 'admin', first_name: 'mentor', last_name: 'performance', 
